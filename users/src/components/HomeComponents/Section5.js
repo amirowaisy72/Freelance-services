7582747bf1a9d5b4eee251e9534f266b575f4477
Amir Owaisy <amirowaisy72@gmail.com>
2023-08-19 23:58:04 -0700

    Update

---
+++ users/src/components/HomeComponents/Section5.js
@@ -0,0 +1,93 @@
+import React from 'react'
+
+const Section5 = () => {
+    return (
+        <><section className="sec-padding section-bgimg-8">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="sec-title-container text-center">
+                            <h5 className="font-weight-4 less-mar-1 line-height-4 text-primary opacity-9">Our Pricing</h5>
+                            <h2 className="font-weight-6 less-mar-1 line-height-5">We Offer Best Prices</h2>
+                            <div className="ce-title-line"></div>
+                            <h6 className="ce-sub-text raleway opacity-7">Top Quality Line offers Simple and Market Best Prices to our Respectable Customers. As being a Best Top Quality Services provider, we always try our Best to give amazingly economical rates, so everyone can easily get benefits by our Best Quality Services and increase their Businesses for sure</h6>
+                        </div>
+                    </div>
+                    <div className="clearfix"></div>
+                    {/* < />!--end title--> */}
+                    <br></br><br></br>
+
+                    <div className="col-md-4">
+                        <div style={{border:'2px solid black',padding:'5px'}} className="ce-feature-box-13 margin-bottom">
+                            <div className="text-box">
+                                <h5>Digitizing</h5>
+                                <h2>Left Chest / Cap</h2><h1 style={{color:'#FF5733'}} className="price">$15</h1><p style={{color:'#FF5733'}}>Flat Fee</p>
+                                <div className="clearfix"></div>
+                                <ul className="features-list">
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Left Chest/Cap Regardless Stitch Count - $15</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Mid Size 5 to 7 Inches - Min $20 to Max $40</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Jacket Back - Min $45 to Max $80</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Minor Editing Free</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Machine / File Format Conversion Free</li>
+                                </ul>
+                                <div className="clearfix"></div>
+                                <br />
+                                <br />
+                                <div className="col-md-12 text-left nopadding"><button className='btn btn-primary'>Order Now</button></div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* < />!--end item--> */}
+
+                    <div className="col-md-4">
+                        <div style={{border:'2px solid black',padding:'5px'}} className="ce-feature-box-13 active margin-bottom">
+                            <div className="text-box">
+                                <h5>Vectorizing</h5>
+                                <h2>Simple Vector <span style={{color:'#ffffff'}}>Design</span></h2><h1 style={{color:'#FF5733'}} className="price">$10</h1><p style={{color:'#FF5733'}}>Minimum</p>
+                                <div className="clearfix"></div>
+                                <ul className="features-list">
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Simple Vector Art - $10</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Complex Vector Art - Min $15 to Max $45</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Advance Vector Art - Min $50 to Max $100</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check " aria-hidden="true"></i>Minor Editing Free</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check " aria-hidden="true"></i>File Format Conversion Free</li>
+                                </ul>
+                                <div className="clearfix"></div>
+                                <br />
+                                <br />
+                                <div className="col-md-12 text-left"><button className='btn btn-primary'>Order Now</button></div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* < />!--end item--> */}
+
+                    <div className="col-md-4">
+                        <div style={{border:'2px solid black',padding:'5px'}} className="ce-feature-box-13 margin-bottom">
+                            <div className="text-box">
+                                <h5>Web Designing</h5>
+                                <h2>Website Designing</h2><h1 style={{color:'#FF5733'}} className="price">$99</h1><p style={{color:'#FF5733'}}>Minimum</p>
+                                <div className="clearfix"></div>
+                                <ul className="features-list">
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Static Website Design - $99-$149</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Website with animations - $150-$249</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check" aria-hidden="true"></i> Dynamic Websites - $299-$499</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check " aria-hidden="true"></i> E-Commerce Websites - $500-$899</li>
+                                    <li style={{fontSize:'11px', borderBottom:'1px solid #ff3300'}}><i className="fa fa-check " aria-hidden="true"></i> Web Applications - $999-$1500</li>
+
+                                </ul>
+                                <div className="clearfix"></div>
+                                <br />
+                                <br />
+                                <div className="col-md-12 text-left"> <button className='btn btn-primary'>Order Now</button></div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* < />!--end item--> */}
+
+                </div>
+            </div>
+        </section><div className="clearfix"></div></>
+    )
+}
+
+export default Section5
